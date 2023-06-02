5c0817a68e1df0abc80c32a7b6e6ad84cdd6cfe1
Anshil P <anshilp@karunya.edu.in>
2023-06-02 15:34:46 +0530

    Initial commit

---
+++ src/main/java/com/sheridan/thedicegame/TheDiceGameApplication.java
@@ -0,0 +1,13 @@
+package com.sheridan.thedicegame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDiceGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheDiceGameApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sheridan/thedicegame/TheDiceGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sheridan.thedicegame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDiceGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
